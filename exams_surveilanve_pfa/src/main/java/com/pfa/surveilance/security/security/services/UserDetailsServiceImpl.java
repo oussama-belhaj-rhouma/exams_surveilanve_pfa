45de114455ffb5ebcbc2cfcbb89f0ea11189a083
oussama b.rhouma <oussama.belhajrhouma@etudiant-fst.utm.tn>
2023-04-27 11:39:46 +0100

    commmit

--- exams_surveilanve_pfa/src/main/java/com/pfa/surveilance/security/security/services/UserDetailsServiceImpl.java
+++ exams_surveilanve_pfa/src/main/java/com/pfa/surveilance/security/security/services/UserDetailsServiceImpl.java
@@ -1,7 +1,26 @@
 package com.pfa.surveilance.security.security.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.pfa.surveilance.security.Models.User;
+import com.pfa.surveilance.security.Repo.UserRepository;
+@Service
 
-public class UserDetailsServiceImpl {
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
 
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
 
 
 
