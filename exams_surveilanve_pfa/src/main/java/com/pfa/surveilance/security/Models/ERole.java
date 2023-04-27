7b534a2a2dcc4acfe490bd3b4ebc0923e6735650
oussama b.rhouma <oussama.belhajrhouma@etudiant-fst.utm.tn>
2023-04-27 11:11:28 +0100

    commmit

---
+++ exams_surveilanve_pfa/src/main/java/com/pfa/surveilance/security/Models/ERole.java
@@ -0,0 +1,7 @@
+package com.pfa.surveilance.security.Models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
--- exams_surveilanve_pfa/src/main/java/com/pfa/surveilance/security/security/jwt/AuthEntryPointJwt.java
+++ exams_surveilanve_pfa/src/main/java/com/pfa/surveilance/security/security/jwt/AuthEntryPointJwt.java
@@ -1,4 +1,11 @@
 package com.pfa.surveilance.security.security.jwt;
 
+
+
+@Configuration
+@EnableGlobalMethodSecurity(
+        // securedEnabled = true,
+        // jsr250Enabled = true,
+        prePostEnabled = true)
 public class AuthEntryPointJwt {
 }
